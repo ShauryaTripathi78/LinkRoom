683d229a30ee637d08783072d2c8d0ab33e107b0
ShauryaTripathi78 <shauryatripaathi78@gmail.com>
2025-12-06 10:36:30 +0530

    deployement variable

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://linkroom.onrender.com" :
 
-    "http://localhost:8000"
+    "http://localhost:3000"
 
 
 export default server;
